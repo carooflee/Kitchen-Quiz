7ff9689d7f0c7ca9e11822c7a37192c2371d06be
carooflee <61561122+carooflee@users.noreply.github.com>
2020-04-18 15:55:52 -0700

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,3 +1,30 @@
+window.onload = function () {
+  let threeMinutes = 60 * 3,
+      display = document.querySelector('#time');
+  startTimer(threeMinutes, display);
+  this.startQuiz();
+};
+
+let sec = 180;
+function startTimer(){
+ 
+      let timer = setInterval(function(){
+          sec--;
+          document.getElementById('time').innerHTML = sec;
+          if (sec < 0) {
+              clearInterval(timer);
+              alert("Time is up!")
+          }
+      }, 1000);
+  }
+
+  if (sec <= 0) {
+    function highScores() {
+      let x = document.getElementById("highScore");
+      x.style.visibility = "visible";
+      } 
+  }
+
 let questionDiv = document.querySelector("#question");
 let questionNumberTracker = 0;
 let points = 0;
@@ -22,7 +49,7 @@ let quizQuestions = [
   {
     question: "Which one is NOT found in a kitchen?",
     choices: ["Grinder", "Convection oven","Metal snake","Tongs"],
-    answer: "Metal Snake"
+    answer: "Metal snake"
   },
   {
     question: "Who is second in command in a kitchen??",
@@ -33,18 +60,19 @@ let quizQuestions = [
 
 
 function createQuestion(question,options) {
-  //Display question
+  
   let h2 = document.createElement('h2');
   let text = document.createTextNode(question); 
   h2.appendChild(text);
   questionDiv.appendChild(h2);
+
   for(i=0;i<options.length;i++) {
-    var button = document.createElement("button");
+    let button = document.createElement("button");
     questionDiv.appendChild(button);
     button.innerHTML = options[i];
     button.addEventListener ("click", verifyAnswer);
   }
-  //Display options
+  
 }
 function verifyAnswer(event) {  
   console.log(event.target.innerText);
@@ -56,12 +84,14 @@ function verifyAnswer(event) {
   } else {
       alert ("This is why you were never good enough for your father.")
       };
-        function failure() {
-        sec -= 10;
+      function failure() {
+        sec -= 25;
+        display = document.querySelector('#time');
         document.getElementById('time').innerHTML= display + sec;
         };
         failure();
     displayNextQuestion();
+    
     }
 
 
@@ -77,28 +107,10 @@ function startQuiz() {
   displayNextQuestion();
 }
 
+$("#submit").on("click", function() {
+  let addScore = document.getElementById("highScores");
+  localStorage.setItem("highScore", points);
+  localStorage.getItem("highScore").innerText;
+})
 
 
-function startTimer(){
-  let sec = 180;
-      let timer = setInterval(function(){
-          sec--;
-          document.getElementById('time').innerHTML = sec;
-          if (sec < 0) {
-              clearInterval(timer);
-              alert("Time is up!")
-          }
-      }, 1000);
-  }
-
-window.onload = function () {
-  let threeMinutes = 60 * 3,
-      display = document.querySelector('#time');
-  startTimer(threeMinutes, display);
-};
-
-
-
-localStorage.setItem("highScore", "totalPoints");
-// Retrieve
-document.getElementById("points").innerHTML = localStorage.getItem("highScore");
